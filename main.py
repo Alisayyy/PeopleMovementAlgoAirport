00257f0535f79d80bb6ed32412c240f7a5438c9e
Alisayyy <alisayang9015@163.com>
2023-09-22 04:50:53 -0400

    integrate main and front, change how to read in airport map info

--- main.py
+++ main.py
@@ -5,36 +5,51 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 import pygame
-import config as config
+import config as cg
+import math
 
 def buildMap():
-    # user input
-    distanceMap = [[0 , 7 , 1 , 6 , 9 , 11, 13, 6 , 13, 12, 18, 19, 21],
-                   [7 , 0 , 6 , 4 , 7 , 9 , 11, 1 , 12, 10, 16, 17, 19],
-                   [1 , 6 , 0 , 5 , 8 , 10, 12, 4 , 10, 9 , 17, 18, 20],
-                   [6 , 4 , 5 , 0 , 3 , 5 , 7 , 3 , 3 , 4 , 12, 13, 15],
-                   [9 , 7 , 8 , 3 , 0 , 2 , 4 , 4 , 5 , 2 , 9 , 10, 12],
-                   [11, 9 , 10, 5 , 2 , 0 , 2 , 7 , 6 , 1 , 7 , 8 , 10],
-                   [13, 11, 12, 7 , 4 , 2 , 0 , 8 , 6 , 1 , 5 ,6 , 8 ],
-                   [6 , 1 , 4 , 3 , 4 , 7 , 8 , 0 , 11, 13, 15, 16, 18],
-                   [13, 12, 10, 3 , 5 , 6 , 6 , 11, 0 , 3 , 14, 15, 17],
-                   [12, 10, 9 , 4 , 2 , 1 , 1 , 13, 3 , 0 , 8 , 9 , 11],
-                   [18, 16, 17, 12, 9 , 7 , 5 , 15, 14, 8 , 0 , 1 , 2 ],
-                   [19, 17, 18, 13, 10, 8 , 6 , 16, 15, 9 , 1 , 0 , 2 ],
-                   [21, 19, 20, 15, 12, 10, 8 , 18, 17, 11, 2 , 2,  0 ]]
-
+    # calculate distance map according to user input
+    numFacilities = len(cg.facilityInfo)
+    distanceMap = np.arange(numFacilities * numFacilities).reshape(numFacilities,numFacilities)
+    for i in range(numFacilities):
+        for j in range(i, numFacilities):
+            x1 = cg.facilityInfo[i][2]
+            y1 = cg.facilityInfo[i][3]
+            x2 = cg.facilityInfo[j][2]
+            y2 = cg.facilityInfo[j][3]
+            d = int(math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)))
+            distanceMap[i][j] = d
+            distanceMap[j][i] = d
+    # initialize airport map and all facilities
     airportMap = g.Graph(distanceMap)
-    # user input
-    facilityNames = ['gate1', 'gate2', 'restroom1', 'restaurant1', 'store1', 'restaurant2', 'restroom2', 'restroom3',
-                     'store2', 'restaurant3', 'baggage claim1', 'baggage claim2', 'exit']
-    idList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    typeList = ['Gate', 'Gate', 'Restroom', 'Food', 'Shopping', 'Food', 'Restroom', 'Restroom', 'Shopping', 'Drink', 'BC', 'BC', 'Exit']
-    maxOccupancyList = [300, 300, 20, 50, 10, 20, 10, 15, 15, 10, 300, 300, 1000]
-    mediumList = [0, 0, 3, 15, 3, 10, 2, 2, 5, 10, 20, 19, 0]
-    varianceList = [0, 0, 1, 4, 1, 4, 1, 1, 1, 4, 4, 4, 0]
-    for i in range(len(facilityNames)):
-        airportMap.addFacility(idList[i], facilityNames[i], typeList[i], maxOccupancyList[i], mediumList[i], varianceList[i])
+
+    for i in range(numFacilities):
+        airportMap.addFacility(i, cg.facilityInfo[i][0], cg.facilityInfo[i][1],
+                               cg.facilityInfo[i][4], cg.facilityInfo[i][5],
+                               cg.facilityInfo[i][6])
+        if cg.facilityInfo[i][1] == 'Counter':
+            airportMap.counter.append(airportMap.facilityList[i])
+        elif cg.facilityInfo[i][1] == 'Security':
+            airportMap.security.append(airportMap.facilityList[i])
+
+    # need a better way of removing edges
     airportMap.connectAllFacilities()
+
+    # remove connection from main to front facilities
+    for i in range(0, 13):
+        for j in range(13, 19):
+            airportMap.removeEdge(i, j)
+    # remove connection from counter to main
+    for i in range(0, 16):
+        airportMap.removeEdge(13, i)
+        airportMap.removeEdge(14, i)
+        airportMap.removeEdge(15, i)
+    # remove connection from security to counter
+    for i in range(13, 19):
+        airportMap.removeEdge(16, i)
+        airportMap.removeEdge(17, i)
+        airportMap.removeEdge(18, i)
     for i in range(0, 10):
         airportMap.removeEdge(10, i)
     airportMap.removeEdge(10, 11)
@@ -43,6 +58,7 @@ def buildMap():
     for i in range(0, 12):
         airportMap.removeEdge(12, i)
     return airportMap
+
 
 def number_of_certain_probability(sequence, probability):
     x = random.uniform(0, 1)
@@ -53,14 +69,17 @@ def number_of_certain_probability(sequence, probability):
             break
     return item
 
+
 def calTimeSpent(medium, variance):
     return abs(int(np.random.normal(medium, variance))) + 1
+
 
 def normalize(probList):
     sumofProb = sum(probList)
     for i in range(len(probList)):
         probList[i] = probList[i] / sumofProb
     return probList
+
 
 def compute_next_location(person, airportMap, t):
     sequence = []
@@ -94,6 +113,9 @@ def compute_next_location(person, airportMap, t):
         else:
             prob_c.append(1)
 
+        if nextFacility.maxOccupancy == None:
+            print(currentFacility.name)
+            print(nextFacility.name)
         # occupancy of current and next facility
         prob_d.append(abs(nextFacility.lastMinNumPeople - nextFacility.maxOccupancy) / nextFacility.maxOccupancy)
 
@@ -118,91 +140,32 @@ def compute_next_location(person, airportMap, t):
             final_prob.append(0)
         else:
             final_prob.append(0.1*prob_a[i] + 0.7*prob_b[i] + 0.1*prob_c[i] + 0.1*prob_d[i])
-    if person.departureTime is not None:
-        final_prob[10] = 0
-        final_prob[11] = 0
-        final_prob[12] = 0
+    # if person.departureTime is not None:
+    #     final_prob[10] = 0
+    #     final_prob[11] = 0
+    #     final_prob[12] = 0
     final_prob = normalize(final_prob)
 
     person.nextFacility = number_of_certain_probability(sequence, final_prob)
     person.facilityPassed.append(person.nextFacility)
     nextFacility = airportMap.facilityList[person.nextFacility]
 
-    person.toBeAppend[0] = airportMap.getDistance(person.currentFacility, person.nextFacility)
+    person.toBeAppend[0] = int(airportMap.getDistance(person.currentFacility, person.nextFacility) / 40)
     person.toBeAppend[1] = calTimeSpent(nextFacility.medium, nextFacility.variance)
 
 
-
 def plotFacilities():
-    screen.blit(myfont.render("gate1", True, (255, 0, 0)), (10, 230))
-    pygame.draw.rect(screen, [255, 0, 0], [10, 250, 50, 50], 2)  # 0
-    screen.blit(myfont.render("restroom1", True, (255, 0, 0)), (80, 230))
-    pygame.draw.rect(screen, [255, 0, 0], [80, 250, 50, 50], 2)  # 2
-    screen.blit(myfont.render("restaurant1", True, (255, 0, 0)), (230, 230))
-    pygame.draw.rect(screen, [255, 0, 0], [230, 250, 50, 50], 2)  # 3
-    screen.blit(myfont.render("store1", True, (255, 0, 0)), (340, 230))
-    pygame.draw.rect(screen, [255, 0, 0], [340, 250, 50, 50], 2)  # 4
-    screen.blit(myfont.render("restaurant2", True, (255, 0, 0)), (430, 320))
-    pygame.draw.rect(screen, [255, 0, 0], [430, 250, 50, 50], 2)  # 5
-    screen.blit(myfont.render("restroom2", True, (255, 0, 0)), (520, 230))
-    pygame.draw.rect(screen, [255, 0, 0], [520, 250, 50, 50], 2)  # 6
-    screen.blit(myfont.render("baggage claim1", True, (255, 0, 0)), (660, 170))
-    pygame.draw.rect(screen, [255, 0, 0], [660, 190, 50, 50], 2)  # 10
-    screen.blit(myfont.render("exit", True, (255, 0, 0)), (740, 230))
-    pygame.draw.rect(screen, [255, 0, 0], [740, 250, 50, 50], 2)  # 12
-    screen.blit(myfont.render("restroom3", True, (255, 0, 0)), (230, 350))
-    pygame.draw.rect(screen, [255, 0, 0], [230, 370, 50, 50], 2)  # 7
-    screen.blit(myfont.render("gate2", True, (255, 0, 0)), (230, 500))
-    pygame.draw.rect(screen, [255, 0, 0], [230, 440, 50, 50], 2)  # 1
-    screen.blit(myfont.render("store2", True, (255, 0, 0)), (340, 70))
-    pygame.draw.rect(screen, [255, 0, 0], [340, 90, 50, 50], 2)  # 8
-    screen.blit(myfont.render("restaurant3", True, (255, 0, 0)), (430, 170))
-    pygame.draw.rect(screen, [255, 0, 0], [430, 190, 50, 50], 2)  # 9
-    screen.blit(myfont.render("baggage claim2", True, (255, 0, 0)), (660, 320))
-    pygame.draw.rect(screen, [255, 0, 0], [660, 340, 50, 50], 2)  # 11
+    for i in range(len(cg.facilityInfo)):
+        screen.blit(myfont.render(cg.facilityInfo[i][0], True, (255, 0, 0)),
+                    (cg.facilityInfo[i][2], cg.facilityInfo[i][3] - 20))
+        pygame.draw.rect(screen, [255, 0, 0], [cg.facilityInfo[i][2], cg.facilityInfo[i][3],
+                                               cg.facilityBoxSize, cg.facilityBoxSize], 2)
 
 def plotPersonPosition(currentPosition):
-    if currentPosition == 0:
-        pygame.draw.circle(screen, person.color, (35 + random.randint(-20, 20), 275 + random.randint(-20, 20)),
+    if currentPosition != -1:
+        pygame.draw.circle(screen, person.color, (cg.facilityInfo[currentPosition][2] + cg.facilityBoxSize/2 + random.randint(-15, 15),
+                                                  cg.facilityInfo[currentPosition][3] + cg.facilityBoxSize/2 + random.randint(-15, 15)),
                            personRadius)
-    elif currentPosition == 1:
-        pygame.draw.circle(screen, person.color, (255 + random.randint(-20, 20), 465 + random.randint(-20, 20)),
-                           personRadius)
-    elif currentPosition == 2:
-        pygame.draw.circle(screen, person.color, (105 + random.randint(-20, 20), 275 + random.randint(-20, 20)),
-                           personRadius)
-    elif currentPosition == 3:
-        pygame.draw.circle(screen, person.color, (255 + random.randint(-20, 20), 275 + random.randint(-20, 20)),
-                           personRadius)
-    elif currentPosition == 4:
-        pygame.draw.circle(screen, person.color, (365 + random.randint(-20, 20), 275 + random.randint(-20, 20)),
-                           personRadius)
-    elif currentPosition == 5:
-        pygame.draw.circle(screen, person.color, (455 + random.randint(-20, 20), 275 + random.randint(-20, 20)),
-                           personRadius)
-    elif currentPosition == 6:
-        pygame.draw.circle(screen, person.color, (545 + random.randint(-20, 20), 275 + random.randint(-20, 20)),
-                           personRadius)
-    elif currentPosition == 7:
-        pygame.draw.circle(screen, person.color, (255 + random.randint(-20, 20), 395 + random.randint(-20, 20)),
-                           personRadius)
-    elif currentPosition == 8:
-        pygame.draw.circle(screen, person.color, (365 + random.randint(-20, 20), 115 + random.randint(-20, 20)),
-                           personRadius)
-    elif currentPosition == 9:
-        pygame.draw.circle(screen, person.color, (455 + random.randint(-20, 20), 215 + random.randint(-20, 20)),
-                           personRadius)
-    elif currentPosition == 10:
-        pygame.draw.circle(screen, person.color, (685 + random.randint(-20, 20), 215 + random.randint(-20, 20)),
-                           personRadius)
-    elif currentPosition == 11:
-        pygame.draw.circle(screen, person.color, (685 + random.randint(-20, 20), 365 + random.randint(-20, 20)),
-                           personRadius)
-    elif currentPosition == 12:
-        pygame.draw.circle(screen, person.color, (765 + random.randint(-20, 20), 275 + random.randint(-20, 20)),
-                           personRadius)
-    else:
-        pass
 
 
 def print_person_info(person):
@@ -217,29 +180,105 @@ def print_person_info(person):
     print('\n')
 
 
+def createPopulation():
+    populationFront = []
+    populationMain = []
+    removedPopulation = []
+    # arrival and connect
+    for a in range(50):
+        populationMain.append(person.Person(a, 1, currentFacility=0, arrivalTime=0, departureTime=150))
+        populationMain[a].enteredTime = populationMain[a].arrivalTime + int(a / cg.num_people_off_plane)
+    # arrival and exit
+    for b in range(50, 150):
+        populationMain.append(person.Person(b, 12, currentFacility=0, arrivalTime=0))
+        populationMain[b].enteredTime = populationMain[b].arrivalTime + int(b / cg.num_people_off_plane)
+    for c in range(150, 350):
+        populationMain.append(person.Person(c, 12, currentFacility=1, arrivalTime=30))
+        populationMain[c].enteredTime = populationMain[c].arrivalTime + int((c-150) / cg.num_people_off_plane)
+    # departure
+    for d in range(350, 550):
+        temp = random.random() > (1 - cg.rate_self_checked)
+        populationFront.append(person.Person(d, 1, checked=temp, departureTime=150))
+        populationFront[d-350].enteredTime = random.randint(0, 50)
+
+    return populationFront, populationMain, removedPopulation
+
+
+def findLeastCounter(counter):
+    least = counter[0]
+    for c in counter:
+        if c.peopleInLine.size() < least.peopleInLine.size():
+            least = c
+    return least
+
+
+def findLeastSecurity(security):
+    least = security[0]
+    for s in security:
+        if s.peopleInLine.size() < least.peopleInLine.size():
+            least = s
+    return least
+
+
 if __name__ == '__main__':
     airportMap = buildMap()
-    population = []
-    removedPopulation = []
-    num_exits = []
-    # TODO： replace with people creation function which can take in flight info
-    for j in range(100):
-        population.append(person.Person(j, 0, 0, 1, 100))
-    for j in range(100, 200):
-        population.append(person.Person(j, 30, 1, 12))
-    # num_people_off_plane = 15  # per min
 
-    for person in population:
-        person.enteredTime = person.arrivalTime + int((person.id % 100) / config.num_people_off_plane)
+    # population = []
+    # removedPopulation = []
+    # num_exits = []
+    # # TODO： replace with people creation function which can take in flight info
+    # for j in range(100):
+    #     population.append(person.Person(j, 0, 0, 1, 100))
+    # for j in range(100, 200):
+    #     population.append(person.Person(j, 30, 1, 12))
+    # # num_people_off_plane = 15  # per min
+    #
+    # for person in population:
+    #     person.enteredTime = person.arrivalTime + int((person.id % 100) / config.num_people_off_plane)
+
+    populationFront, populationMain, removedPopulation = createPopulation()
+
 
     t = 0
-    for t in range(150):
+    for t in range(200):
+        # front
+        for person in populationFront:
+            if t >= person.enteredTime:
+                if person.currentFacility == None and not person.checked:
+                    c = findLeastCounter(airportMap.counter)
+                    c.peopleInLine.enqueue(person)
+                    person.currentFacility = c.id
+                elif person.currentFacility == None and person.checked:
+                    s = findLeastSecurity(airportMap.security)
+                    s.peopleInLine.enqueue(person)
+                    person.currentFacility = s.id
+                person.movementTrack.append(person.currentFacility)
+
+        for c in airportMap.counter:
+            if not c.peopleInLine.empty():
+                c.timeCounter += 1
+                if c.timeCounter == cg.check_time:
+                    person = c.peopleInLine.dequeue()
+                    person.currentFacility = None
+                    person.checked = True
+                    c.timeCounter = 0
+        for s in airportMap.security:
+            if not s.peopleInLine.empty():
+                s.timeCounter += 1
+                if s.timeCounter == cg.security_time:
+                    person = s.peopleInLine.dequeue()
+                    #person.currentFacility = None
+                    s.timeCounter = 0
+                    populationMain.append(person)
+                    populationFront.remove(person)
+
         for facility in airportMap.facilityList:
             airportMap.facilityList[facility].lastMinNumPeople = len(airportMap.facilityList[facility].people)
             airportMap.facilityList[facility].people = []
-
-        for person in population:
+        # main
+        for person in populationMain:
             if t >= person.enteredTime:
+                # still in hallway
                 if person.toBeAppend[0] != 0:
                     person.currentFacility = -1
                     person.toBeAppend[0] -= 1
@@ -251,6 +290,8 @@ if __name__ == '__main__':
                         person.toBeAppend[1] -= 1
                     if person.currentFacility != 12 and person.currentFacility != person.destination and person.toBeAppend[1] == 0:
                         compute_next_location(person, airportMap, t)
+
+                # append current position
                 person.movementTrack.append(person.currentFacility)
 
                 if person.currentFacility != -1:
@@ -277,8 +318,8 @@ if __name__ == '__main__':
         # num_exits.append(len(airportMap.facilityList[12].people))
 
         for person in removedPopulation:
-            if person in population:
-                population.remove(person)
+            if person in populationMain:
+                populationMain.remove(person)
 
     # plt.plot([sum(num_exits[:k]) for k in range(len(num_exits))])
     # plt.savefig('cumulative.jpg')
@@ -289,9 +330,9 @@ if __name__ == '__main__':
     # print('sum of people exiting', sum(num_exits))
     for person in removedPopulation:
         print_person_info(person)
-    for person in population:
+    for person in populationMain:
         print_person_info(person)
-    print('remainPopulation: ', len(population))
+    print('remainPopulation: ', len(populationMain))
     print('removedPopulation:', len(removedPopulation))
 
 
