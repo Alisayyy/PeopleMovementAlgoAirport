9ce597c3fd3640d139534f63da8617beb4e0bfdb
Alisayyy <alisayang9015@163.com>
2023-11-02 07:51:13 -0400

    minor changes

--- person.py
+++ person.py
@@ -1,20 +1,22 @@
 import random
-import config as config
+import config as cg
 
 class Person:
 
-    def __init__(self, id, destination, currentFacility=None, arrivalTime=None, checked=None, departureTime=None):
+    def __init__(self, id, destination, currentFacility=None, arrivalTime=None, checked=None, departureTime=None, enteredTime = None):
         self.id = id
-        self.enteredTime = None
+        self.enteredTime = enteredTime
         self.exitTime = None
         #self.flightID = flightID
+        self.speed = random.randint(cg.minWalkingSpeed, cg.maxWalkingSpeed)
+
 
         self.currentFacility = currentFacility
         self.nextFacility = None
         self.destination = destination
         self.movementTrack = []
         self.toBeAppend = [0, 0]  # time to be appended as hallway and facility
-        self.facilityPassed = []
+        self.facilityPassed = set()
 
         self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))  # for visualization
 
